475de61df7729e9eca706ccf0423b858f6319282
Vivek Subash Nair <130990985+vsn-5968@users.noreply.github.com>
2025-01-19 19:52:31 +0530

    binary_decimal

--- binary_decimal.py
+++ binary_decimal.py
@@ -2,4 +2,4 @@ binary=input("Enter a binary number: ")
 decimal=0
 for i in binary:
     decimal=decimal*2+int(i)
-print("Equivlent decimal number is: ",decimal)
+print("\nEquivlent decimal number is: ",decimal)
