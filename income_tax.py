4983cf46b83f33cb222b498412b067b880e28d8e
Vivek Subash Nair <130990985+vsn-5968@users.noreply.github.com>
2025-01-19 16:42:34 +0530

    income_tax

---
+++ income_tax.py
@@ -0,0 +1,28 @@
+income=int(input("Enter the annual salary of the user: "))
+if income<=300000:
+    print("No tax")
+    print("Net income: ",income)
+elif income>300000 and income<=700000:
+    tax=(income-250000)*0.05
+    print("Tax paid is 5%")
+    income=income-tax
+    print("Net income: ",income)
+elif income>700000 and income<=1000000:
+    tax=20000+(income-500000)*0.1
+    print("Tax paid is 10%")
+    income=income-tax
+    print("Net income: ",income)
+elif income>1000000 and income<=1200000:
+    tax=50000+(income-750000)*0.15
+    print("Tax paid is 15%")
+    income=income-tax
+    print("Net income: ",income)
+elif income>1200000 and income<=1500000:
+    tax=80000+(income-1000000)*0.2
+    print("Tax paid is 20%")
+    income=income-tax
+    print("Net income: ",income)
+else:
+    tax=140000+(income-1500000)*0.3
+    print("Tax paid is 30%")
+    print("Net income: ",income)
